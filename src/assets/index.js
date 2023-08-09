2c6943a13087d6954a10c4109d8b604fdaae2ae5
commitEvil <thegen.jackson@gmail.com>
2023-08-09 19:26:17 +1000

    fixes
    more fixes

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,13 +1,6 @@
-import clipIcon from './icon_clip.png'
-import loader from './loader.svg'
-import copy from './icon_copy.png'
 import logo from './LogoHorizontal.png'
-import tick from './icon_check.png'
+
 
 export {
-    clipIcon,
-    loader,
-    copy,
-    logo,
-    tick
+    logo
 }
